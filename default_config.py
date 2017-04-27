19399cbc7a3c98fff2b5fe6542ea0a06f82856a6
hijkzzz <janhu9527@gmail.com>
2017-04-27 11:35:58 +0800

    fix

--- default_config.py
+++ default_config.py
@@ -1,4 +1,4 @@
-import os
+# coding=utf-8
 
 CELERY_BROKER_URL = 'redis://localhost:6379/0'
 CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
@@ -7,14 +7,14 @@ MAIL_SERVER = 'smtp.qq.com'
 MAIL_PROT = 25
 MAIL_USE_TLS = True
 MAIL_USE_SSL = False
-MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+MAIL_USERNAME = ''
+MAIL_PASSWORD = ''
 MAIL_DEBUG = True
 
 UPLOAD_FOLDER = 'raw-images/'
-ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
+ALLOWED_EXTENSIONS = set(['.png', '.jpg', '.jpeg'])
 MAX_CONTENT_LENGTH = 4 * 1024 * 1024
 
 MODEL_FOLDER = 'models/'
-MODEL_FILES = set(['fast-style-transfer.ckpt-done'])
+MODEL_FILES = set(['fast-style-transfer.cpkt'])
 OUTPUT_FOLDER = 'output-images/'
--- server.py
+++ server.py
@@ -2,28 +2,37 @@
 from flask import Flask, request, jsonify
 from flask_mail import Mail, Message
 from celery import Celery
-from os import mkdir, remove 
+from os import mkdir, remove, environ
 from os.path import join, exists, splitext
 import re
 import time
 import base64
 import commands
+import sys
 
-app = Flask(__name__, instance_relative_config=True)
+# 解决编码问题
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+app = Flask(__name__)
 app.config.from_pyfile('default_config.py')
+app.config['MAIL_USERNAME'] = environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = environ.get('MAIL_PASSWORD')
 mail = Mail(app)
-celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
-celery.conf.update(app.config)
+celery = Celery(
+    app.name, broker=app.config['CELERY_BROKER_URL'], backend=app.config['CELERY_RESULT_BACKEND'])
 
 
 @app.route('/transform', methods=['POST'])
 def transform():
     # 获取参数
-    json_data = request.get_json()
+    json_data = request.get_json() 
     filename = json_data.get('filename')
     image = json_data.get('image')
     email = json_data.get('email')
     model = json_data.get('model')
+
+    print json_data
 
     # 检查参数
     if filename is None or image is None or email is None or model is None:
@@ -52,28 +61,33 @@ def transform_async(filename, image, email, model):
     with open(join(app.config['UPLOAD_FOLDER'], filename), 'wb') as f:
         f.write(image)
 
-    # 转换图片 
+    # 转换图片
     content_file_path = join(app.config['UPLOAD_FOLDER'], filename)
     model_file_path = join(app.config['MODEL_FOLDER'], model)
     output_folder = app.config['OUTPUT_FOLDER']
+    output_file_path = join(output_folder, filename)
     command = 'python eval.py --CONTENT_IMAG %s --MODEL_PATH %s -- OUTPUT_FOLDER %s' % (
         content_file_path, model_file_path, output_folder)
     status, output = commands.getstatusoutput(command)
 
+    print status, output
+
     # 发送邮件
     if status == 0:
-        msg = Message("IMAGE-STYLE-TRANSFER", sender=app.config['MAIL_USERNAME'], recipients=[email])
+        msg = Message("IMAGE-STYLE-TRANSFER",
+                      sender=app.config['MAIL_USERNAME'], recipients=[email])
         msg.body = filename
-        output_file_path = join(output_folder, filename)
         with app.open_resource(output_file_path) as f:
-            mime_type = 'image/jpg' if splitext(filename)[1] is not 'png' else 'image/png'
+            mime_type = 'image/jpg' if splitext(
+                filename)[1] is not '.png' else 'image/png'
             msg.attach(filename, mime_type, f.read())
         with app.app_context():
             mail.send(msg)
     else:
         pass
 
-    remove_files.apply_async(args=[[content_file_path, output_file_path]], countdown=180)
+    remove_files.apply_async(
+        args=[[content_file_path, output_file_path]], countdown=60)
 
 
 @celery.task
