1b5db827454b567d08733c0e0be7649de038399d
hijkzzz <janhu9527@gmail.com>
2017-04-27 14:43:03 +0800

    encode

--- default_config.py
+++ default_config.py
@@ -4,10 +4,10 @@ CELERY_BROKER_URL = 'redis://localhost:6379/0'
 CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
 
 MAIL_SERVER = 'smtp.qq.com'
-MAIL_PROT = 25
+MAIL_PROT = 465 
 MAIL_USE_TLS = True
 MAIL_USE_SSL = False
-MAIL_USERNAME = ''
+MAIL_USERNAME = 'hujia_n@foxmail.com'
 MAIL_PASSWORD = ''
 MAIL_DEBUG = True
 
@@ -16,5 +16,5 @@ ALLOWED_EXTENSIONS = set(['.png', '.jpg', '.jpeg'])
 MAX_CONTENT_LENGTH = 4 * 1024 * 1024
 
 MODEL_FOLDER = 'models/'
-MODEL_FILES = set(['fast-style-transfer.cpkt'])
+MODEL_FILES = set(['mosaic.ckpt'])
 OUTPUT_FOLDER = 'output-images/'
--- server.py
+++ server.py
@@ -2,37 +2,32 @@
 from flask import Flask, request, jsonify
 from flask_mail import Mail, Message
 from celery import Celery
-from os import mkdir, remove, environ
+from os import mkdir, remove
 from os.path import join, exists, splitext
 import re
 import time
 import base64
 import commands
-import sys
 
-# 解决编码问题
-reload(sys)
-sys.setdefaultencoding('utf-8')
 
 app = Flask(__name__)
 app.config.from_pyfile('default_config.py')
-app.config['MAIL_USERNAME'] = environ.get('MAIL_USERNAME')
-app.config['MAIL_PASSWORD'] = environ.get('MAIL_PASSWORD')
 mail = Mail(app)
 celery = Celery(
-    app.name, broker=app.config['CELERY_BROKER_URL'], backend=app.config['CELERY_RESULT_BACKEND'])
+    app.name, broker=app.config['CELERY_BROKER_URL'])
+celery.conf.update(app.config)
 
 
 @app.route('/transform', methods=['POST'])
 def transform():
     # 获取参数
-    json_data = request.get_json() 
-    filename = json_data.get('filename')
-    image = json_data.get('image')
-    email = json_data.get('email')
-    model = json_data.get('model')
+    json_data = request.get_json()
+    filename = json_data.get(u'filename').encode('ascii')
+    image = json_data.get(u'image').encode('ascii')
+    email = json_data.get(u'email').encode('ascii')
+    model = json_data.get(u'model').encode('ascii')
 
-    print json_data
+    print len(image), filename, email, model
 
     # 检查参数
     if filename is None or image is None or email is None or model is None:
@@ -49,19 +44,19 @@ def transform():
     except TypeError:
         return jsonify(status='IMAGE ERROR')
 
-    # 异步转换
-    transform_async.delay(filename, image, email, model)
-    return jsonify(status='SUBMIT_SUCCESS')
-
-
-@celery.task
-def transform_async(filename, image, email, model):
     # 保存图片
     filename = str(time.time()) + '-' + filename
     with open(join(app.config['UPLOAD_FOLDER'], filename), 'wb') as f:
         f.write(image)
 
-    # 转换图片
+    # 异步转换
+    transform_async.delay(filename, email, model)
+    return jsonify(status='SUBMIT_SUCCESS')
+
+
+@celery.task
+def transform_async(filename, email, model):
+    # 开始转换 
     content_file_path = join(app.config['UPLOAD_FOLDER'], filename)
     model_file_path = join(app.config['MODEL_FOLDER'], model)
     output_folder = app.config['OUTPUT_FOLDER']
@@ -74,14 +69,14 @@ def transform_async(filename, image, email, model):
 
     # 发送邮件
     if status == 0:
-        msg = Message("IMAGE-STYLE-TRANSFER",
-                      sender=app.config['MAIL_USERNAME'], recipients=[email])
-        msg.body = filename
-        with app.open_resource(output_file_path) as f:
-            mime_type = 'image/jpg' if splitext(
-                filename)[1] is not '.png' else 'image/png'
-            msg.attach(filename, mime_type, f.read())
         with app.app_context():
+            msg = Message("IMAGE-STYLE-TRANSFER",
+                        sender=app.config['MAIL_USERNAME'], recipients=[email])
+            msg.body = filename
+            with app.open_resource(output_file_path) as f:
+                mime_type = 'image/jpg' if splitext(
+                    filename)[1] is not '.png' else 'image/png'
+                msg.attach(filename, mime_type, f.read())
             mail.send(msg)
     else:
         pass
@@ -103,4 +98,4 @@ if __name__ == '__main__':
     if not exists(app.config['OUTPUT_FOLDER']):
         mkdir(app.config['OUTPUT_FOLDER'])
 
-    app.run()
+    app.run(debug=False)
